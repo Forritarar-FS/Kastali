7a7097000112276ca98797502ef7d8ea85de4a01
Palli <icekhaos@hotmail.com>
2014-11-10 10:12:06 +0000

    Accurate diagonals in turtle via binary numbers!

---
+++ pythonHus/speedsolve.py
@@ -0,0 +1,12 @@
+import turtle
+
+speedscreen = turtle.Screen()	
+speedsolve = turtle.Turtle()
+
+speedsolve.speed(1)
+
+speedsolve.forward(256)
+speedsolve.right(128)
+speedsolve.forward(64)
+speedsolve.left(32)
+speedsolve.backward(16)
